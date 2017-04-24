586dcb33c71a8c64a49fcfa241853f7bd61d2dda
albertgomez <albertgomez@localhost>
2017-04-24 11:57:25 +0200

    add test

---
+++ src/Bowling/BowlingKataTest.java
@@ -0,0 +1,14 @@
+package Bowling;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BowlingKataTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
