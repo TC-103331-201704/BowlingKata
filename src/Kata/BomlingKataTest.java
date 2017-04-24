52be3ade43417efda8be28e599341e98f6e8c6c5
xasear <xevi232@gmail.com>
2017-04-24 11:56:17 +0200

    Add Test

---
+++ src/Kata/BomlingKataTest.java
@@ -0,0 +1,14 @@
+package Kata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BomlingKataTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
