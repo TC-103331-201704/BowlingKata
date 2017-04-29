832730c568e3e3d4529decf747dd3b3c711fc917
PereCastilloJapon <pcastillo@edu.tecnocampus.cat>
2017-04-29 11:57:47 +0200

    second test 7

--- src/bowling/BowlingKataTest.java
+++ src/bowling/BowlingKataTest.java
@@ -11,16 +11,18 @@ private Game g;
 		g=new Game();
 	}
 	
-	@Test
-	public void test() {
-		rollMany(20,0);
-			assertEquals(0,g.score());
-	}
 	private void rollMany(int n, int pins){
 		for(int i=0;i<n;i++){
 			g.roll(pins);
 		}
 	}
+	
+	@Test
+	public void test() {
+		rollMany(20,0);
+			assertEquals(0,g.score());
+	}
+
 	@Test
 	public void testAllOnes(){
 		rollMany(20,1);
