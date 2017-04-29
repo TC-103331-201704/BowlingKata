7d7c03ea65f4c42cbc190b9e19826339ccfcbd44
PereCastilloJapon <pcastillo@edu.tecnocampus.cat>
2017-04-29 12:14:14 +0200

    third test 3

--- src/bowlingGame/Game.java
+++ src/bowlingGame/Game.java
@@ -2,8 +2,11 @@ package bowlingGame;
 
 public class Game {
 	private int score=0;
+	private int rolls[]= new int[21];
+	private int currentRoll=0;
 	public void roll(int pins){
 		score+=pins;
+		rolls[currentRoll++]=pins;
 	}
 	public int score(){
 		return score;
