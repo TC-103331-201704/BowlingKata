9d9bf4f4ad78cdb7ca4bd53324d382a6f0c22ccc
PereCastilloJapon <pcastillo@edu.tecnocampus.cat>
2017-04-29 12:19:48 +0200

    third test 5

--- src/bowling/BowlingKataTest.java
+++ src/bowling/BowlingKataTest.java
@@ -29,14 +29,14 @@ private Game g;
 		assertEquals(20,g.score());
 	}
 	
-	//@Test
-	//public void testOneSpare()throws Exception{
-		//g.roll(5);
-		//g.roll(5);
-		//g.roll(3);
-		//rollMany(17,0);
-		//assertEquals(16,g.score());
-	//}
+	@Test
+	public void testOneSpare()throws Exception{
+		g.roll(5);
+		g.roll(5);
+		g.roll(3);
+		rollMany(17,0);
+		assertEquals(16,g.score());
+	}
 	
 
 }
