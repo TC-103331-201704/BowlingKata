1675a798ba595476f543d9c256ebe08595c7940c
PereCastilloJapon <pcastillo@edu.tecnocampus.cat>
2017-04-29 11:52:00 +0200

    second test 2

--- src/bowling/BowlingKataTest.java
+++ src/bowling/BowlingKataTest.java
@@ -1,23 +1,24 @@
 package bowling;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 import bowlingGame.Game;
+import junit.framework.TestCase;
 
-public class BowlingKataTest {
-
+public class BowlingKataTest extends TestCase {
+private Game g;
+	public void setUp() throws Exception{
+		g=new Game();
+	}
+	
 	@Test
 	public void test() {
-		Game g=new Game();
 		for(int i=0; i<20;i++){
 			g.roll(0);}
 			assertEquals(0,g.score());
 	}
 	@Test
 	public void testAllOnes(){
-		Game g=new Game();
 		for(int i=0;i<20;i++){
 			g.roll(1);
 		}
