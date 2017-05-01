de2246b6286ad791c8d0c490ac8fe17a43c51295
alexh <aharo@edu.tecnocampus.cat>
2017-05-01 21:26:17 +0200

    The third test, 46.

--- src/BowlingKataTest.java
+++ src/BowlingKataTest.java
@@ -31,11 +31,16 @@ public class BowlingKataTest {
 
 	@Test
 	public void testOneSpare() throws Exception {
-		g.roll(5);
-		g.roll(5);// spare
+		rollSpare();
 		g.roll(3);
 		rollMany(17, 0);
 		assertEquals(16, g.score());
+	}
+
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
+		
 	}
 
 }
