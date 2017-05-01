63a54995f9818ad33e1cfbc495afd6c0cf2a6264
alexh <aharo@edu.tecnocampus.cat>
2017-05-01 20:27:42 +0200

    The second test, 25.

--- src/BowlingKataTest.java
+++ src/BowlingKataTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 public class BowlingKataTest {
 
-	
 	private Game g;
 
 	@Before
@@ -17,10 +16,13 @@ public class BowlingKataTest {
 	public void testGutterGame() throws Exception {
 		int n = 20;
 		int pins = 0;
-		for (int i = 0; i < n; i++) {
-			g.roll(pins);
-		}
+		rollMany(n, pins);
 		assertEquals(0, g.score());
+	}
+
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++)
+			g.roll(pins);
 	}
 
 	@Test
