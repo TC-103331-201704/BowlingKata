0f55f3ef3b721af7a8d2ac584f3a47b920216c50
ignasiargemipuig <ignasiargemi@gmail.com>
2017-05-01 19:19:18 +0200

    Second Test

--- src/bowling/BowlingKataTest.java
+++ src/bowling/BowlingKataTest.java
@@ -10,12 +10,35 @@ import static junit.framework.TestCase.fail;
  */
 public class BowlingKataTest extends TestCase{
 
-    @Test
-    public void testGutterGame() {
-        //fail("Not implemented yet.");
-        Game g = new Game();
-        for (int i=0; i<20; i++)
-            g.roll(0);
+    private Game g;
+    protected void setUp() throws Exception {
+        g = new Game();
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
+
+        @Test
+    public void testGutterGame() throws Exception{
+        rollMany(20, 0);
         assertEquals(0, g.score());
     }
+
+    @Test
+    public void testAllOnes() throws Exception {
+        rollMany(20, 1);
+        assertEquals(20, g.score());
+    }
+
+    /*@Test
+    public void testOneSpare() throws Exception {
+        g.roll(5);
+        g.roll(5); // spare
+        g.roll(3);
+        rollMany(17,0);
+        assertEquals(16,g.score());
+    }*/
+
 }
--- src/bowling/Game.java
+++ src/bowling/Game.java
@@ -5,14 +5,15 @@ package bowling;
  */
 public class Game {
 
+    private int score = 0;
 
     public Game() {}
 
     public void roll(int pins) {
-
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
