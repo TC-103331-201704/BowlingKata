37f09f96ab3ae11d66039372c6a67fc4fad753ee
Oriol Mateos <oriol_mat@hotmail.com>
2017-05-02 12:09:41 +0200

    TEST - ROLL METHOD INSTANCED

--- src/test/bowling.java
+++ src/test/bowling.java
@@ -6,5 +6,7 @@ import junit.framework.TestCase;
 public class bowling extends TestCase{
 	public void testGutterGame() throws Exception{
 		Game g = new Game();
+		
+		for(int i = 0; i<20; i++) g.roll(0);
 	}
 }
