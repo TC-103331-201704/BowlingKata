1bd891896a880c612771413302913ab10f1c61f9
Oriol Mateos <oriol_mat@hotmail.com>
2017-05-02 12:35:59 +0200

    GAME - PAIR SCORE CONTROLL

--- src/code/Game.java
+++ src/code/Game.java
@@ -12,10 +12,12 @@ public class Game {
 	
 	public int score(){
 		int score = 0;
+		int i = 0;
 		
-		for(int i = 1 ; i<this.rolls.length ; i++){
-			if(this.rolls[i] + this.rolls[i+1] == 10) score+=10;
-			else score += this.rolls[i];
+		for(int frame = 0; frame<10 ; frame++){
+			score+= rolls[i] + rolls[i+1];
+			
+			i+=2;
 		}
 		
 		return score;
