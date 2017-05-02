080305b94b13eab9b9d65a301db74dba61e5fcdd
Oriol Mateos <oriol_mat@hotmail.com>
2017-05-02 12:28:01 +0200

    GAME - SAVE OLD ROLLS

--- src/code/Game.java
+++ src/code/Game.java
@@ -2,8 +2,13 @@ package code;
 
 public class Game {
 	private int score = 0;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
 	
-	public void roll(int pins){ this.score+=pins;}
+	public void roll(int pins){
+		this.score+=pins;
+		this.rolls[currentRoll++] = pins;
+	}
 	
 	public int score(){return this.score;}
 }
