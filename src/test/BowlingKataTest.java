bad3dc0a9a2669793a183eefed0f3b146de45bdc
Jordi R <jordi@shiraz.es>
2017-05-02 19:33:09 +0200

    Third test fails.

--- src/test/BowlingKataTest.java
+++ src/test/BowlingKataTest.java
@@ -29,6 +29,15 @@ public class BowlingKataTest {
 		setUp();
 	    rollMany(20, 1);
 	    assertEquals(20, g.score());
-	  }
+	}
+	
+	@Test
+	public void testOneSpare() throws Exception {
+	    g.roll(5);
+	    g.roll(5); // spare
+	    g.roll(3);
+	    rollMany(17,0);
+	    assertEquals(16,g.score());
+	}
 
 }
