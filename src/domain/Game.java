f3a0a16f3957564971c72b676a7878f89bd04a21
Jordi R <jordi@shiraz.es>
2017-05-02 19:38:45 +0200

    Added Game variables.

--- src/domain/Game.java
+++ src/domain/Game.java
@@ -3,9 +3,12 @@ package domain;
 public class Game {
 	
 	private int score = 0;
+	private int[] rolls = new int[22];
+	private int currentRoll = 0;
 	
 	public void roll(int pins){
 		score += pins;
+		rolls[currentRoll++] = pins;
 	}
 	
 	public int score(){
