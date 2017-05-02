2db9f67d36e3e9f5ac032d473a4a087e6212d0a9
Jordi R <jordi@shiraz.es>
2017-05-02 19:40:22 +0200

    Refactor, score is calculated in method.

--- src/domain/Game.java
+++ src/domain/Game.java
@@ -2,16 +2,17 @@ package domain;
 
 public class Game {
 	
-	private int score = 0;
 	private int[] rolls = new int[22];
 	private int currentRoll = 0;
 	
 	public void roll(int pins){
-		score += pins;
 		rolls[currentRoll++] = pins;
 	}
 	
 	public int score(){
-		return score;
+	    int score = 0;
+	    for (int i = 0; i < rolls.length; i++)
+	    	score += rolls[i];
+	    return score;
 	}
 }
