ca782478076a53cb8792f9d6e8787a06ac771e43
Oriol Mateos <oriol_mat@hotmail.com>
2017-05-02 12:15:07 +0200

    TEST - OPTIMIZE REPEATED GAME INSTANCES

--- src/test/bowling.java
+++ src/test/bowling.java
@@ -4,18 +4,19 @@ import code.Game;
 import junit.framework.TestCase;
 
 public class bowling extends TestCase{
-	public void testGutterGame() throws Exception{
-		Game g = new Game();
-		
+	private Game g;
+	
+	protected void setUp() throws Exception{this.g = new Game();}
+	
+	
+	public void testGutterGame() throws Exception{		
 		for(int i = 0; i<20; i++) g.roll(0);
 		
 		assertEquals(0, g.score());
 	}
 	
 	
-	public void testAllOnes() throws Exception{
-		Game g = new Game();
-		
+	public void testAllOnes() throws Exception{		
 		for(int i = 0; i<20; i++) g.roll(1);
 
 		assertEquals(20, g.score());
