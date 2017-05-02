76c94889f27b98a580ffc698459192d0c14ca28a
Jordi R <jordi@shiraz.es>
2017-05-02 19:01:13 +0200

    First test fails.

---
+++ src/test/BowlingKataTest.java
@@ -0,0 +1,13 @@
+package test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BowlingKataTest {
+
+	@Test
+	public void testGutterGame() {
+		Game game = new Game();
+	}
+
+}
