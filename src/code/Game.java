daab824f115478c99553ebedbc50f49285148945
Oriol Mateos <oriol_mat@hotmail.com>
2017-05-02 12:38:27 +0200

    GAME - MOOVED COMMENT

--- src/code/Game.java
+++ src/code/Game.java
@@ -15,7 +15,8 @@ public class Game {
 		int i = 0;
 		
 		for(int frame = 0; frame<10 ; frame++){
-			if (rolls[i] + rolls[i + 1] == 10){ //SPARE
+			//SPARE
+			if (rolls[i] + rolls[i + 1] == 10){
 				score += 10 + rolls[i + 2];
 				i += 2;
 			}else{
