d1b964d22e5312cf1630e49d9fd25068ad1cd643
rmc6789 <marticabrera94@gmail.com>
2017-05-02 17:42:43 +0200

    changed roll and score methods in Game class. Also in BowlingGameTest class, the Game object creation is no longer duplicated

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,9 +1,13 @@
 import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase{
+	private Game g;
+	
+	protected void setUp() throws Exception{
+		g = new Game();
+	}
+	
 	public void testGutterGame() throws Exception{
-		Game g = new Game();
-		
 		for (int i = 0; i < 20; i++){
 			g.roll(0);
 		}
@@ -12,8 +16,6 @@ public class BowlingGameTest extends TestCase{
 	}
 	
 	public void testAllOnes() throws Exception{
-		Game g = new Game();
-		
 		for(int i = 0; i < 20; i++){
 			g.roll(1);
 		}
