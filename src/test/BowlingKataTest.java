4ef493e9487cfac4605b09859e390ffa0af7a2c1
Jordi R <jordi@shiraz.es>
2017-05-02 19:18:16 +0200

    Single Game variable.

--- src/test/BowlingKataTest.java
+++ src/test/BowlingKataTest.java
@@ -5,21 +5,25 @@ import org.junit.Test;
 import domain.Game;
 
 public class BowlingKataTest {
-
+	
+	private Game g;
+	
+    protected void setUp() throws Exception {
+    	g = new Game();
+	}
+	
 	@Test
 	public void testGutterGame() {
-		Game game = new Game();
 	    for (int i=0; i<20; i++)
-	    	game.roll(0);
-	    assertEquals(0, game.score());
+	    	g.roll(0);
+	    assertEquals(0, g.score());
 	}
 	
 	@Test
     public void testAllOnes() throws Exception {
-	    Game game = new Game();
 	    for (int i = 0; i < 20; i++)
-	    	game.roll(1);
-	    assertEquals(20, game.score());
+	    	g.roll(1);
+	    assertEquals(20, g.score());
 	  }
 
 }
