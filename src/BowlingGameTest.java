3e837676ae78fbbd80b28f9aad60a9b69f39760d
xos30 <xaviorse@hotmail.com>
2017-05-03 20:39:58 +0200

    New situation

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -4,6 +4,12 @@ import junit.framework.TestCase;
  * Created by xos on 03-May-17.
  */
 public class BowlingGameTest extends TestCase {
+    private Game g;
+
+    protected void setUp() throws Exception {
+        g = new Game();
+    }
+
     public void testGutterGame() throws Exception {
         Game g = new Game();
         for (int i=0; i<20; i++)
--- src/Game.java
+++ src/Game.java
@@ -2,10 +2,13 @@
  * Created by xos on 03-May-17.
  */
 public class Game {
-    public void roll(int pins) {
+    private int score = 0;
 
+    public void roll(int pins) {
+        score+= pins;
     }
+
     public int score() {
-        return 0;
+        return score;
     }
 }
