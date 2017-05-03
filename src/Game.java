df1826cf9a6d8bdeae9e54741a4a39c5d159ea17
Sharon Cabrera <sharoncabc@gmail.com>
2017-05-03 22:57:10 +0200

    Third test.11

--- src/Game.java
+++ src/Game.java
@@ -15,8 +15,6 @@ public class Game {
         int score = 0;
 
         for (int i = 0; i < rolls.length; i++)
-            if (rolls[i] + rolls[i+1] == 10) // spare
-               // score +=
             score += rolls[i];
         return score;
     }
