3944bf7cceb22766ee3194c6b88138d116ca6923
zapperosa <zapperosa@gmail.com>
2017-05-03 17:29:18 +0200

    third test(4)

--- src/bowlingGame/Game.java
+++ src/bowlingGame/Game.java
@@ -4,17 +4,16 @@ public class Game {
 	
 	private int rolls[] = new int[21];
 	private int currentRoll = 0;
-	private int score = 0;
+
 	
 	public void roll(int pins){
-		score+=pins;
 		rolls[currentRoll++] = pins;
 	}
 	
 	public int score(){
 		int score = 0;
 		for (int i = 0; i < rolls.length; i++)
-		score += rolls[i];
+			score += rolls[i];
 		return score;
 	}
 
