c9c4d32fe57737d26a298ab8da16dee6eb9c2cf2
Sharon Cabrera <sharoncabc@gmail.com>
2017-05-03 22:20:19 +0200

    BowlingGame class and BowlingGameTest have been created

---
+++ Tests/BowlingGameTest.java
@@ -0,0 +1,8 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by Sharon on 03/05/2017.
+ */
+public class BowlingGameTest extends TestCase {
+
+}
---
+++ src/BowlingGame.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Sharon on 03/05/2017.
+ */
+public class BowlingGame {
+}
