65feff09faf3e0036d092bc340aaa83e9965a33a
xos30 <xaviorse@hotmail.com>
2017-05-03 20:56:19 +0200

    New situation

--- src/Game.java
+++ src/Game.java
@@ -14,8 +14,6 @@ public class Game {
     public int score() {
         int score = 0;
         for (int i=0; i<rolls.length; i++)
-            if (rolls[i] + rolls[i+1] == 10) //spare
-                score += ...
             score += rolls[i];
         return score;
     }
