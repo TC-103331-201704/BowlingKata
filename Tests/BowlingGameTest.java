8ddd133b632b4390119c1709b19e0e6da0186ee3
Sharon Cabrera <sharoncabc@gmail.com>
2017-05-03 22:43:57 +0200

    Second test.9 (rollMany method has been moved)

--- Tests/BowlingGameTest.java
+++ Tests/BowlingGameTest.java
@@ -10,15 +10,16 @@ public class BowlingGameTest extends TestCase {
     protected void setUp () throws Exception {
         g = new Game();
     }
+    private void rollMany(int n, int pins){
+        for (int i = 0; i < n; i++) g.roll(pins);
+    }
 
     public void testGutterGame() throws Exception{
         rollMany(20, 0);
         assertEquals(0,g.score());
     }
 
-    private void rollMany(int n, int pins){
-        for (int i = 0; i < n; i++) g.roll(pins);
-    }
+
 
     public void testAllOnes() throws  Exception {
         rollMany(20, 1);
