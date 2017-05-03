152c26ccb7b4252718723b86bcc073cd355df9aa
Khostaxz <alvarocostasanchez@gmail.com>
2017-05-04 00:33:39 +0200

    The First Test. (1)

---
+++ src/testing/BowlingGameTest.java
@@ -0,0 +1,10 @@
+package testing;
+
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+	
+	public void testGutterGame() throws Exception {
+		 Game g = new Game();
+	}
+}
