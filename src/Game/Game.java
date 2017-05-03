f3e6c24925cc1321493be612eaacab1ae67e289a
santi <sherranzm@edu.tecnocampus.cat>
2017-05-03 14:43:31 +0200

    Second test

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -4,4 +4,14 @@ package Game;
  * Created by santi on 3/5/17.
  */
 public class Game {
+
+    private int score = 0;
+
+    public void roll(int pins) {
+        score += pins;
+    }
+
+    public int score() {
+        return score;
+    }
 }
--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -5,9 +5,20 @@ import junit.framework.TestCase;
  * Created by santi on 3/5/17.
  */
 public class BowlingGameTest extends TestCase {
+    private Game g;
 
-    public void testGutterGame() throws Exception{
-        Game g = new Game();
+    protected void setUp() throws Exception {
+        g = new Game();
     }
 
+    public void testGutterGame() throws Exception{
+        for (int i=0; i<20; i++)
+            g.roll(0);
+        assertEquals(0, g.score());
+    }
+    public void testAllOnes() throws Exception {
+        for (int i = 0; i < 20; i++)
+            g.roll(1);
+        assertEquals(20, g.score());
+    }
 }
