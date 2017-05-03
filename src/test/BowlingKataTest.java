99d1d5c3cc82aa6d5aea64602fac358296096f30
Jordi R <jordi@shiraz.es>
2017-05-03 09:55:03 +0200

    Fourth test fails.

--- src/test/BowlingKataTest.java
+++ src/test/BowlingKataTest.java
@@ -43,5 +43,14 @@ public class BowlingKataTest {
 	    rollMany(17,0);
 	    assertEquals(16,g.score());
 	}
+	
+	@Test
+	public void testOneStrike() throws Exception {
+	    g.roll(10); // strike
+	    g.roll(3);
+	    g.roll(4);
+	    rollMany(16, 0);
+	    assertEquals(24, g.score());
+	  }
 
 }
