5f15ca5e13aac3b179d36e906055ebd3364dda90
zapperosa <zapperosa@gmail.com>
2017-05-03 17:15:15 +0200

    first test(2)

--- src/bowlingGame/Game.java
+++ src/bowlingGame/Game.java
@@ -1,5 +1,11 @@
 package bowlingGame;
 
 public class Game {
+	
+	public void roll(int npins){}
+	
+	public int score(){
+		return 0;
+	}
 
 }
--- src/bowlingTest/BowlingKataTest.java
+++ src/bowlingTest/BowlingKataTest.java
@@ -10,6 +10,10 @@ public class BowlingKataTest extends TestCase {
 
 	public void testGutterGame() throws Exception{
 		Game g = new Game();
-	}
+		for (int i = 0; i < 20; ++i){
+			g.roll(0);
+		}
+		assertEquals(0, g.score());
+	}	
 
 }
