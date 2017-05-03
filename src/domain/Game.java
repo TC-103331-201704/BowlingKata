7a834725d358a431a5ab8c23a24179af85d725fc
Jordi Adan <jordiadan@MacBook-Pro-de-Jordi.local>
2017-05-03 18:30:47 +0200

    Second Test - 2

--- src/domain/Game.java
+++ src/domain/Game.java
@@ -4,10 +4,11 @@ package domain;
  * Created by jordiadan on 3/5/17.
  */
 public class Game {
+    private int score = 0;
     public void roll(int pins) {
+        score += pins;
     }
-
     public int score() {
-        return 0;
+        return score;
     }
 }
--- src/test/BowlingGameTest.java
+++ src/test/BowlingGameTest.java
@@ -7,18 +7,18 @@ import junit.framework.TestCase;
  * Created by jordiadan on 3/5/17.
  */
 public class BowlingGameTest extends TestCase {
+    private Game g;
+    protected void setUp() throws Exception {
+        g = new Game();
+    }
     public void testGutterGame() throws Exception {
-        Game g = new Game();
-        for (int i=0; i<20; i++)
+        for (int i = 0; i < 20; i++)
             g.roll(0);
         assertEquals(0, g.score());
     }
-
     public void testAllOnes() throws Exception {
-        Game g = new Game();
         for (int i = 0; i < 20; i++)
             g.roll(1);
         assertEquals(20, g.score());
     }
-
 }
