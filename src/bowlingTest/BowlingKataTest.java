4f0bf2d91b97fb576823df92a94b569a3b604fcb
zapperosa <zapperosa@gmail.com>
2017-05-03 17:26:37 +0200

    third test

--- src/bowlingTest/BowlingKataTest.java
+++ src/bowlingTest/BowlingKataTest.java
@@ -17,15 +17,23 @@ public class BowlingKataTest extends TestCase {
 	private void rollMany(int n, int pins) {
 		 for (int i = 0; i < n; i++)
 		 g.roll(pins);
-		 }
+	}
 	
 	public void testGutterGame() throws Exception {
 		 rollMany(20, 0);
 		 assertEquals(0, g.score());
-		 }
+	}
 		 
 	public void testAllOnes() throws Exception {
 		 rollMany(20, 1);
 		 assertEquals(20, g.score());
-		 }
+	}
+	
+	//public void testOneSpare() throws Exception {
+		// g.roll(5);
+		 //g.roll(5); // spare
+		 //g.roll(3);
+		 //rollMany(17,0);
+		 //assertEquals(16,g.score());
+		 //}
 }
