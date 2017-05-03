ed2fb1b35e0121c982bcdb6059ce4fd6b6a102a5
Khostaxz <alvarocostasanchez@gmail.com>
2017-05-04 00:57:55 +0200

    The Second Test. (3)

--- src/testing/BowlingGameTest.java
+++ src/testing/BowlingGameTest.java
@@ -4,20 +4,23 @@ import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
 	
-	 private Game g;
-	 
-	 protected void setUp() throws Exception {
-		 g = new Game();
-	 }
-	 
-	 public void testGutterGame() throws Exception {
-		 for (int i = 0; i < 20; i++)
-			 g.roll(0);
-		 assertEquals(0, g.score());
-	 }
-	 public void testAllOnes() throws Exception {
-		 for (int i = 0; i < 20; i++)
-			 g.roll(1);
-		 assertEquals(20, g.score());
-	 }
+	private Game g;
+ 
+	protected void setUp() throws Exception {
+		g = new Game();
+	}
+ 
+	public void testGutterGame() throws Exception {
+		int n = 20;
+		int pins = 0;
+		for (int i = 0; i < n; i++)
+			g.roll(pins);
+		assertEquals(0, g.score());
+	}
+	
+	public void testAllOnes() throws Exception {
+		for (int i = 0; i < 20; i++)
+			g.roll(1);
+		assertEquals(20, g.score());
+	}
 }
