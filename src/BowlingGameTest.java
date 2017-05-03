df9027f25f04e25e20e5bab5aa74599f63d506b5
xos30 <xaviorse@hotmail.com>
2017-05-03 20:30:33 +0200

    Situation 1.1

---
+++ src/BowlingGameTest.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by xos on 03-May-17.
+ */
+public class BowlingGameTest extends TestCase {
+    public void testGutterGame() throws Exception {
+        Game g = new Game();
+    }
+}
