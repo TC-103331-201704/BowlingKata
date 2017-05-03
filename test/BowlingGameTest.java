726f3ee7323ec534d346042f9d483c5cfd46dfb1
santi <sherranzm@edu.tecnocampus.cat>
2017-05-03 14:51:40 +0200

    Third test

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -6,19 +6,29 @@ import junit.framework.TestCase;
  */
 public class BowlingGameTest extends TestCase {
     private Game g;
-
     protected void setUp() throws Exception {
         g = new Game();
     }
-
-    public void testGutterGame() throws Exception{
-        for (int i=0; i<20; i++)
-            g.roll(0);
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
+    public void testGutterGame() throws Exception {
+        rollMany(20, 0);
         assertEquals(0, g.score());
     }
     public void testAllOnes() throws Exception {
-        for (int i = 0; i < 20; i++)
-            g.roll(1);
+        rollMany(20,1);
         assertEquals(20, g.score());
+    }
+    public void testOneSpare() throws Exception {
+        rollSpare();
+        g.roll(3);
+        rollMany(17,0);
+        assertEquals(16,g.score());
+    }
+    private void rollSpare() {
+        g.roll(5);
+        g.roll(5);
     }
 }
