e45211fa798ca86220faa5437647048858857175
Khostaxz <alvarocostasanchez@gmail.com>
2017-05-04 01:13:16 +0200

    The Third Test. (7)

--- src/testing/Game.java
+++ src/testing/Game.java
@@ -12,6 +12,8 @@ public class Game {
 	public int score() {
 		int score = 0;
 		for (int i = 0; i < rolls.length; i++)
+			if (rolls[i] + rolls[i+1] == 10) // spare
+				 score += ...
 			score += rolls[i];
 		return score;
 	} 
