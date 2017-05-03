b504b256eab192dea2741a8661a0e9aadcadf967
zapperosa <zapperosa@gmail.com>
2017-05-03 17:44:34 +0200

    fourth test(4)

--- src/bowlingTest/BowlingKataTest.java
+++ src/bowlingTest/BowlingKataTest.java
@@ -34,7 +34,19 @@ public class BowlingKataTest extends TestCase {
 		 g.roll(3);
 		 rollMany(17,0);
 		 assertEquals(16,g.score());
-		 }
+	}
+	
+	public void testOneStrike() throws Exception {
+		 rollStrike();
+		 g.roll(3);
+		 g.roll(4);
+		 rollMany(16, 0);
+		 assertEquals(24, g.score());
+	}
+	
+	private void rollStrike() {
+		 g.roll(10);
+	}
 	
 	private void rollSpare() {
 		 g.roll(5);
