4809bdba43b2c660b81fc741a00fec14338d4c2b
Sergi Bernaus <sergibernaus@yahoo.es>
2017-05-04 12:55:21 +0200

    First test 1

---
+++ test/BowlingGameTest.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by Sergi on 04/05/2017.
+ */
+public class BowlingGameTest extends TestCase{
+    public void testGutterGame() {
+        Game g = new Game();
+    }
+}
