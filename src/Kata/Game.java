1f792929283082ba0aaf12fa4c4d0676900102af
xasear <xevi232@gmail.com>
2017-05-04 13:10:49 +0200

    roll current role Game third test

--- src/Kata/Game.java
+++ src/Kata/Game.java
@@ -1,12 +1,16 @@
 package Kata;
 
 public class Game {
- private int score = 0;
- 
- public void roll(int pins) {
-	 score += pins;
- }
- public int score() {
-	 return score;
- }
+	private int score = 0;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
+
+	public void roll(int pins) {
+		score += pins;
+		rolls[currentRoll++] = pins;
+	}
+
+	public int score() {
+		return score;
+	}
 }
