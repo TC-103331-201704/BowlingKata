c6cc2cf74985aaabb6d5f3729bb3bed1881c8991
Sergi Bernaus <sergibernaus@yahoo.es>
2017-05-04 13:23:00 +0200

    Second test 2

--- src/Game.java
+++ src/Game.java
@@ -2,10 +2,13 @@
  * Created by Sergi on 04/05/2017.
  */
 public class Game {
+    private int score = 0;
+
     public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
