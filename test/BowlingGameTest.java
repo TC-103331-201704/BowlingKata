9accec7406139a52bcf5e458f16fa820b655b55c
Sergi Bernaus <sergibernaus@yahoo.es>
2017-05-04 13:02:44 +0200

    First test 3

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -6,5 +6,8 @@ import junit.framework.TestCase;
 public class BowlingGameTest extends TestCase{
     public void testGutterGame() {
         Game g = new Game();
+        for (int i = 0; i < 20; i++){
+            g.roll(0);
+        }
     }
 }
