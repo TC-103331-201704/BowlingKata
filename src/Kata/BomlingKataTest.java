48e16115eacde7762f57727f0e3e5de76fa7599f
xasear <xevi232@gmail.com>
2017-05-04 12:25:52 +0200

    test 1 success diapo 3

--- src/Kata/BomlingKataTest.java
+++ src/Kata/BomlingKataTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import junit.framework.TestCase;
+
 public class BomlingKataTest extends TestCase {
 	public void testGutterGame() throws Exception {
 		 Game g = new Game();
