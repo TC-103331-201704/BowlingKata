b0de250d09fdf534f31cdb57b672a904d0eab5bc
Sergi Bernaus <sergibernaus@yahoo.es>
2017-05-04 13:20:35 +0200

    Second test 1

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -11,4 +11,12 @@ public class BowlingGameTest extends TestCase{
         }
         assertEquals(0, g.score());
     }
+
+    public void testAllOnes() {
+        Game g = new Game();
+        for (int i = 0; i < 20; i++){
+            g.roll(1);
+        }
+        assertEquals(20, g.score());
+    }
 }
