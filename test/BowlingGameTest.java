b124ad010b9b349ba02df9a3b7788de324a6d71c
Sergi B <sergibernaus@yahoo.es>
2017-05-08 10:33:25 +0000

    Second test 3

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -4,14 +4,21 @@ import junit.framework.TestCase;
  * Created by Sergi on 04/05/2017.
  */
 public class BowlingGameTest extends TestCase{
+    private Game g;
+
+    protected void setUp(){
+        g = new Game();
+    }
+
     public void testGutterGame() {
-        Game g = new Game();
-        for (int i = 0; i < 20; i++){
-            g.roll(0);
+        int n = 20;
+        int pins = 0;
+        for (int i = 0; i < n; i++){
+            g.roll(pins);
         }
         assertEquals(0, g.score());
     }
-
+    
     public void testAllOnes() {
         Game g = new Game();
         for (int i = 0; i < 20; i++){
