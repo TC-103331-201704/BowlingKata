e01b5823dc99a443f15e1ef6d372b9360909a97e
Sergi B <sergibernaus@yahoo.es>
2017-05-08 10:41:59 +0000

    Third test 1

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -10,19 +10,27 @@ public class BowlingGameTest extends TestCase{
         g = new Game();
     }
 
-    public void testGutterGame() {
-        rollMany(20, 0);
-        assertEquals(0, g.score());
-    }
-
     private void rollMany(int n, int pins) {
         for (int i = 0; i < n; i++) {
             g.roll(pins);
         }
     }
-    
+
+    public void testGutterGame() {
+        rollMany(20, 0);
+        assertEquals(0, g.score());
+    }
+
     public void testAllOnes() {
         rollMany(20, 1);
         assertEquals(20, g.score());
+    }
+
+    public void testOneSpare() {
+        g.roll(5);
+        g.roll(5); //Spare
+        g.roll(3);
+        rollMany(17, 0);
+        assertEquals(16, g.score());
     }
 }
