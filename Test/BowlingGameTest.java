1c4c7b1c25ec62d6def6dd3ef8ab9d88526d765b
alejandromoneomartinez <alejandromoneo13@gmail.com>
2017-05-10 18:42:24 +0200

    3 test

--- Test/BowlingGameTest.java
+++ Test/BowlingGameTest.java
@@ -14,4 +14,12 @@ public class BowlingGameTest {
             g.roll(0);
         assertEquals(0, g.score());
     }
+
+    @Test
+    public void testAllOnes(){
+        Game g = new Game();
+        for (int i=0; i<20; i++)
+            g.roll(i);
+        assertEquals(20, g.score());
+    }
 }
