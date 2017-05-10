9330ff5063bfc03cdad9ab056650d6e6db495c12
alejandromoneomartinez <alejandromoneo13@gmail.com>
2017-05-10 18:58:53 +0200

    10 test

--- Test/BowlingGameTest.java
+++ Test/BowlingGameTest.java
@@ -30,4 +30,13 @@ public class BowlingGameTest {
         rollMany(20,1);
         assertEquals(20, g.score());
     }
+
+    @Test
+    public void testOneSpare() throws Exception {
+        g.roll(5);
+        g.roll(5); // spare
+        g.roll(3);
+        rollMany(17,0);
+        assertEquals(16,g.score());
+    }
 }
