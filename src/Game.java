1da00d3f63a05458fa45666f7b64e71c2d84a7d6
alejandromoneomartinez <alejandromoneo13@gmail.com>
2017-05-10 19:51:48 +0200

    23 test

--- src/Game.java
+++ src/Game.java
@@ -4,17 +4,14 @@
 public class Game {
     private int rolls[] = new int[21];
     private int currentRoll = 0;
-
     public void roll(int pins) {
         rolls[currentRoll++] = pins;
     }
-
     public int score() {
         int score = 0;
         int frameIndex = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (rolls[frameIndex] == 10) // strike
-            {
+            if (isStrike(frameIndex)) {
                 score += 10 + strikeBonus(frameIndex);
                 frameIndex++;
             } else if (isSpare(frameIndex)) {
@@ -27,20 +24,19 @@ public class Game {
         }
         return score;
     }
-
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
+    }
     private int sumOfBallsInFrame(int frameIndex) {
-        return rolls[frameIndex] + rolls[frameIndex + 1];
+        return rolls[frameIndex] + rolls[frameIndex+1];
     }
-
     private int spareBonus(int frameIndex) {
-        return rolls[frameIndex + 2];
+        return rolls[frameIndex+2];
     }
-
     private int strikeBonus(int frameIndex) {
-        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        return rolls[frameIndex+1] + rolls[frameIndex+2];
     }
-
     private boolean isSpare(int frameIndex) {
-        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        return rolls[frameIndex]+rolls[frameIndex+1] == 10;
     }
 }
