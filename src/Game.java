16ecccec442552cdaaec8b60fba3baee33862bc1
alejandromoneomartinez <alejandromoneo13@gmail.com>
2017-05-10 19:01:07 +0200

    12 test

--- src/Game.java
+++ src/Game.java
@@ -3,12 +3,15 @@
  */
 public class Game {
     private int score = 0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        this.score += pins;
+        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
-        return this.score;
+        return score;
     }
 }
