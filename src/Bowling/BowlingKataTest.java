c324e629ab5c9f85b4e86594bdc1d00637299217
Albert Gomez <agomezr@edu.tecnocampus.cat>
2017-05-16 09:33:12 +0200

    Ordenate by usable methods

--- src/Bowling/BowlingKataTest.java
+++ src/Bowling/BowlingKataTest.java
@@ -10,20 +10,20 @@ public class BowlingKataTest extends TestCase {
 
 	}
 
-	public void testGutterGame() throws Exception {
-		
-		rollMany(20, 0);
-		assertEquals(0, g.score());
-
-	}
-
 	private void rollMany(int n, int pins) {
 		for (int i = 0; i < n; i++)
 			g.roll(pins);
 	}
 
+	public void testGutterGame() throws Exception {
+
+		rollMany(20, 0);
+		assertEquals(0, g.score());
+
+	}
+
 	public void testAllOnes() throws Exception {
-		rollMany(20,1);
+		rollMany(20, 1);
 		assertEquals(20, g.score());
 	}
 }
