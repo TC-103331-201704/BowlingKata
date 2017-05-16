6633058167c0fff57c61143a23ef552fd92c2833
Albert Gomez <agomezr@edu.tecnocampus.cat>
2017-05-16 09:25:26 +0200

    Finishing the second test, creating the score variable and making clean code

--- src/Bowling/BowlingKataTest.java
+++ src/Bowling/BowlingKataTest.java
@@ -3,13 +3,23 @@ package Bowling;
 import junit.framework.TestCase;
 
 public class BowlingKataTest extends TestCase {
+	private Game g;
 
-	public void testGutterGame() throws Exception{
-		Game g = new Game();
-		for(int i=0;i<20 ;i++)
+	protected void setUp() {
+		g = new Game();
+
+	}
+
+	public void testGutterGame() throws Exception {
+		for (int i = 0; i < 20; i++)
 			g.roll(0);
 		assertEquals(0, g.score());
-		
+
+	}
+
+	public void testAllOnes() throws Exception {
+		for (int i = 0; i < 20; i++)
+			g.roll(1);
+		assertEquals(20, g.score());
 	}
 }
- 
