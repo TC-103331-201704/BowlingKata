1c0d28b0015f9dfd5fc74751e182d5dd88cc967b
Albert Gomez <agomezr@edu.tecnocampus.cat>
2017-05-16 10:19:24 +0200

    Making the condition strike in game class

--- src/Bowling/BowlingKataTest.java
+++ src/Bowling/BowlingKataTest.java
@@ -32,8 +32,9 @@ public class BowlingKataTest extends TestCase {
 		rollMany(17, 0);
 		assertEquals(16, g.score());
 	}
+
 	public void testOneStrike() throws Exception {
-		g.roll(10);//strike
+		g.roll(10);// strike
 		g.roll(3);
 		g.roll(4);
 		rollMany(16, 0);
