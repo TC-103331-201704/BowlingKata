1860d82527639b15744da41b206738e358257d48
Albert Gomez <agomezr@edu.tecnocampus.cat>
2017-05-16 09:28:57 +0200

    Creating the rollMany method

--- src/Bowling/BowlingKataTest.java
+++ src/Bowling/BowlingKataTest.java
@@ -5,16 +5,22 @@ import junit.framework.TestCase;
 public class BowlingKataTest extends TestCase {
 	private Game g;
 
-	protected void setUp() {
+	protected void setUp() throws Exception {
 		g = new Game();
 
 	}
 
 	public void testGutterGame() throws Exception {
-		for (int i = 0; i < 20; i++)
-			g.roll(0);
+		int n = 20;
+		int pins = 0;
+		rollMany(n, pins);
 		assertEquals(0, g.score());
 
+	}
+
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++)
+			g.roll(pins);
 	}
 
 	public void testAllOnes() throws Exception {
