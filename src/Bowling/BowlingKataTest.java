81c3e28f07b47645f8d1e7f4415f2fe9c39b4ce8
Albert Gomez <agomezr@edu.tecnocampus.cat>
2017-05-16 09:18:48 +0200

    Create score method

--- src/Bowling/BowlingKataTest.java
+++ src/Bowling/BowlingKataTest.java
@@ -6,9 +6,10 @@ public class BowlingKataTest extends TestCase {
 
 	public void testGutterGame() throws Exception{
 		Game g = new Game();
-		for(int i=0;i<20 ;i++){
+		for(int i=0;i<20 ;i++)
 			g.roll(0);
-		}
+		assertEquals(0, g.score());
+		
 	}
 }
  
