fe8abdbcabb0c7f0510367116acdb890d10752f7
Cyumus <carlosbg1995@gmail.com>
2017-05-16 18:04:42 +0200

    + First test

--- src/testing/BowlingGameTest.java
+++ src/testing/BowlingGameTest.java
@@ -1,6 +1,10 @@
 package testing;
 
+import game.Game;
 import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {	
+	public void testGutterGame() throws Exception {
+		Game g = new Game();
+	}
 }
