5122ded160f31307e1cc093ed4fb9a52c4f5ce13
Albert Gomez <agomezr@edu.tecnocampus.cat>
2017-05-16 09:14:40 +0200

    "Solucionant possibles errors de git"

--- src/Bowling/BowlingKataTest.java
+++ src/Bowling/BowlingKataTest.java
@@ -1,13 +1,14 @@
 package Bowling;
 
-import org.junit.Test;
-
 import junit.framework.TestCase;
 
 public class BowlingKataTest extends TestCase {
 
 	public void testGutterGame() throws Exception{
 		Game g = new Game();
+		for(int i=0;i<20 ;i++){
+			g.roll(0);
+		}
 	}
 }
  
