efed380d367c9f46a2ece11bef6dea19f13ed03e
eloirodriguezgaxas <erodriguezg@edu.tecnocamous.cat>
2017-05-17 13:31:14 +0200

    Diapositiva 9

--- src/tests/BowlingKataTest.java
+++ src/tests/BowlingKataTest.java
@@ -22,10 +22,22 @@ public class BowlingKataTest {
 	}
 	
 	@Test
+	public void testGutterGame() throws Exception {
+		int n = 20;
+		int pins = 0;
+		for (int i = 0; i < n; i++) {
+			g.roll(pins);
+	    }
+		assertEquals(0, g.score());
+	}
+
+	@Test
 	public void testAllOnes() throws Exception {
 		for (int i = 0; i < 20; i++)
 			g.roll(1);
 		assertEquals(20, g.score());
 	}
+	
+
 
 }
