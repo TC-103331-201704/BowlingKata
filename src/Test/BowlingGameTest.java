7cf4aa0ea07adb04741d839ee8d90836b45e801b
Cristina <crisvido@yahoo.es>
2017-05-18 20:13:05 +0200

    Second Test 2 and changes in Game class

--- src/Test/BowlingGameTest.java
+++ src/Test/BowlingGameTest.java
@@ -5,21 +5,22 @@ import junit.framework.TestCase;
 import Domain.*;
 
 public class BowlingGameTest extends TestCase{
+	private Game g;
+	
+	protected void setUp() throws Exception {
+		g = new Game();
+	}
 
 	public void testGutterGame() throws Exception {
-		Game g = new Game();
-		for (int i=0; i<20; i++){
+		for (int i=0; i<20; i++)
 			g.roll(0);
 		assertEquals(0, g.score());
-		}
 	}
 	
 	public void testAllOnes() throws Exception {
-		Game g = new Game();
-		for (int i = 0; i < 20; i++){
+		for (int i = 0; i < 20; i++)
 			g.roll(1);
-		assertEquals(20, g.score());
-		}
+		assertEquals(20, g.score());	
 	}
 
 }
