4ec41e00694b173071757c901f8c158a62a6da9a
Cristina <crisvido@yahoo.es>
2017-05-18 20:01:07 +0200

    First Test 1

---
+++ src/Test/BowlingGameTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import junit.framework.TestCase;
+
+import Domain.*;
+
+public class BowlingGameTest extends TestCase{
+
+	public void testGutterGame() throws Exception {
+		Game g = new Game();
+	}
+
+}
