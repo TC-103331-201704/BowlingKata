3d502dec03b7cc01d7adedcd0af27ea5d79a3572
eloirodriguezgaxas <erodriguezg@edu.tecnocamous.cat>
2017-05-21 10:01:37 +0200

    Diapositiva 19

--- src/tests/BowlingKataTest.java
+++ src/tests/BowlingKataTest.java
@@ -38,12 +38,12 @@ public class BowlingKataTest {
 	    assertEquals(20, g.score());
 	}
 	
-	  public void testOneSpare() throws Exception {
-	    g.roll(5);
-	    g.roll(5);
-	    g.roll(3);
-	    rollMany(17,0);
-	    assertEquals(16,g.score());
-	  }
+//	  public void testOneSpare() throws Exception {
+//	    g.roll(5);
+//	    g.roll(5);
+//	    g.roll(3);
+//	    rollMany(17,0);
+//	    assertEquals(16,g.score());
+//	  }
 	
 }
