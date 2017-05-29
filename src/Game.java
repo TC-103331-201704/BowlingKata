8fc3ea00432e8500e64181c9758cc987475d6680
Raul Garcia Brota <raulgarbro29@gmail.com>
2017-05-29 11:12:13 +0200

    Fourth test

--- src/Game.java
+++ src/Game.java
@@ -10,8 +10,9 @@ public class Game {
         int score = 0;
         int frameIndex = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (rolls[frameIndex] == 10) // strike
-            {
+            if (
+                    isStrike(frameIndex)
+                    ) {
                 score += 10 + strikeBonus(frameIndex);
                 frameIndex++;
             } else if (isSpare(frameIndex)) {
@@ -23,6 +24,10 @@ public class Game {
             }
         }
         return score;
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
     }
 
     private int sumOfBallsInFrame(int frameIndex) {
