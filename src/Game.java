09d26d09c40f314a388f74c2ebe58ef8d8ebaee0
Raul Garcia Brota <raulgarbro29@gmail.com>
2017-05-29 11:03:38 +0200

    Third test

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,15 @@
 public class Game {
-    private int score = 0;
     private int rolls[] = new int[21];
     private int currentRoll = 0;
     public void roll(int pins) {
-        score += pins;
         rolls[currentRoll++] = pins;
     }
     public int score() {
         int score = 0;
-        for (int i = 0; i < rolls.length; i++)
+        for (int i = 0; i < rolls.length; i++)  {
+            if (rolls[i] + rolls[i+1] == 10) // spare score += ...
             score += rolls[i];
+        }
         return score;
     }
 }
