0a487128f9ffbdc2e65340d34d318da4644db7c6
Raul Garcia Brota <raulgarbro29@gmail.com>
2017-05-29 10:38:14 +0200

    First test.

---
+++ src/BowlingGameTest.java
@@ -0,0 +1,8 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class BowlingGameTest extends TestCase {
+    public void testGutterGame() throws Exception {
+        Game g = new Game();
+    }
+}
