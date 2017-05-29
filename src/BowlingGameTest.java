218ad0db1602ab4860d40dbe543261dd13b98488
Raul Garcia Brota <raulgarbro29@gmail.com>
2017-05-29 10:53:29 +0200

    Second test.

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,21 +1,19 @@
 import junit.framework.TestCase;
-
 public class BowlingGameTest extends TestCase {
     private Game g;
-
     protected void setUp() throws Exception {
         g = new Game();
     }
-
     public void testGutterGame() throws Exception {
         int n = 20;
         int pins = 0;
-        for (int i = 0; i < n; i++) {
-            g.roll(pins);
-        }
+        rollMany(n, pins);
         assertEquals(0, g.score());
     }
-
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
     public void testAllOnes() throws Exception {
         for (int i = 0; i < 20; i++)
             g.roll(1);
